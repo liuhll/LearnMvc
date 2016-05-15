a0e88e1dbbedb990f89e4f975cc42cf7986ba938
liuhongliang <1029765111@qq.com>
2016-05-15 22:34:55 +0800

    移除genlelella包，添加Bower以及bower.json，项目的前端框架将通过bower来管理

--- src/WebUI/App_Start/BundleConfig.cs
+++ src/WebUI/App_Start/BundleConfig.cs
@@ -4,26 +4,39 @@ namespace WebUI.App_Start
 {
     public class BundleConfig
     {
+        private const string GentelellaJsSrc = "~/Content/gentelella-1.1.0/js/{0}";
+        private const string GentelellaCssSrc = "";
+        private const string BootstrapJsSrc = "";
+        private const string BootstrapCssSrc = "";
+
+
         public static void RegisterBundles(BundleCollection bundles)
         {
             //jquery
             bundles.Add(new ScriptBundle("~/scripts/jquery").Include(
                "~/scripts/jquery-{version}.js"));
 
-            //ace-extra
-            bundles.Add(new ScriptBundle("~/scripts/ace/js/ace-extra").Include(
-                "ace-extra.min.js"));
+            //nprogress
+            bundles.Add(new ScriptBundle(string.Format(GentelellaJsSrc, "nprogress")).Include(
+                string.Format(GentelellaJsSrc, "nprogress.js")));
+
 
             //bootstrap
             bundles.Add(new ScriptBundle("~/scripts/ace/js/bootstrap").Include(
                 ""));
 
-            //page specific plugin scripts
-            bundles.Add(new ScriptBundle("~/scripts/ace/js/plugin-scripts").Include(
-                "bootstrap-tag.min.js",
-                "jquery.hotkeys.min.js",
-                "bootstrap-wysiwyg.min.js"));
-            
+            //gauge
+            bundles.Add(new ScriptBundle("~/scripts/ace/js/gauge").Include(
+                "gauge.min.js"));
+
+            //bootstrap_progress 
+            bundles.Add(new ScriptBundle("~/scripts/ace/js/bootstrap-progress").Include(
+                "bootstrap-progressbar.min.js"));
+
+            //icheck
+            bundles.Add(new ScriptBundle("~/scripts/ace/js/icheck").Include(
+                "icheck"));
+
             //ace plugin
             bundles.Add(new ScriptBundle("~/scripts/ace/js/ace-plugin-scripts").Include(
                 "ace-elements.min.js",
