533004339ea4240cb830f98ee42fc90613567131
liuhongliang <1029765111@qq.com>
2016-05-19 20:58:16 +0800

    Add Entities

---
+++ src/LearnMvc.Domain/Entities/Action.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnMvc.Domain.Entities
+{
+    public class Action
+    {
+        public Guid Id { get; set; }
+
+        public string ActionName { get; set; }
+
+        public virtual IList<ActionColumn> ActionColumn { get; set; }
+
+    }
+}
