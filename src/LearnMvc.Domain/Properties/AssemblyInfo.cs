6712bf7171962611cbbe65553a36c38cc5fd395f
liuhongliang <1029765111@qq.com>
2016-05-19 20:44:17 +0800

    reset this projetc library type

--- src/LearnMvc.Domain/Properties/AssemblyInfo.cs
+++ src/LearnMvc.Domain/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -14,7 +13,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a1b9c8f7-9bc9-453c-b7e1-da1d5591cb1e")]
 
 // Version information for an assembly consists of the following four values:
 //
